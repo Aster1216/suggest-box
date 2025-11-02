a4e389d68dcfbd5229445b5c50ff354a2e53d5a4
Aster  Demewez <159171409+Aster1216@users.noreply.github.com>
2025-11-02 22:49:50 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,11 @@
 const mongoose = require('mongoose');
 
 async function connectDB(uri) {
-  if (!uri) {
-    console.error('❌ MongoDB URI is missing!');
-    process.exit(1);
-  }
-
   try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('✅ MongoDB Atlas Connected');
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("✅ MongoDB Connected");
   } catch (err) {
-    console.error('❌ Mongo connection failed:', err.message);
+    console.error("Mongo connection failed:", err.message);
     process.exit(1);
   }
 }
